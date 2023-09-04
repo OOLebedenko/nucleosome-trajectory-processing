f8248431744c8a406126eea80b8521ec9a2d5f18
OOLebedenko <oolebedenko@gmail.com>
2023-09-04 14:59:03 +0300

    refactor the 15N relaxation common.mk

--- 15N_relaxation_rates/analysis_template/H4_tails/common.mk
+++ 15N_relaxation_rates/analysis_template/H4_tails/common.mk
@@ -1,30 +1,30 @@
 ## 1. change the path to SCRIPT_DIR according to your directory tree
 SCRIPT_DIR:={YOUR PATH}/nucleosome-trajectory-processing/15N_relaxation_rates/scripts/
 
-## 2. specify MD traj parameters
-# set trajectory path; for example md_setup/md_protocol/TIP4P-D_disp-replica-1/ff99SB-disp/wt/02_production/6_run/
+## 2. specify MD trajectory parameters
+# set trajectory path; for example, md_setup/md_protocol/TIP4P-D_disp/ff99SB-disp/wt/02_production/6_run/
 TRAJECTORY_PATH:=""
-# set path to reference pdb (of note, we need ); for example md_protocol/TIP4P-D_disp-replica-1/ff99SB-disp/wt/01_equil_histone_tails/0_prepare/wt.pqr
+# set path to reference pdb (of note, chains should be labeled same as in 3LZ0); for example, md_protocol/TIP4P-D_disp/ff99SB-disp/wt/01_equil_histone_tails/0_prepare/wt.pqr
 REFERENCE_PDB_PATH:=""
-# set trajectory length
+# set trajectory length to be processed
 TRAJECTORY_LENGTH:="" # ns
-# set type of trajectory files "dat" - TrjtoolDatFile; "nc" - AmberNetCDF, "xtc" - GromacsXtcFile
+# set type of trajectory files: "dat" - TrjtoolDatFile; "nc" - AmberNetCDF; "xtc" - GromacsXtcFile
 FILETYPE:=nc
-# set pattern of trajectory files files: run00001.dat ---> "run%05d".dat
+# set pattern for trajectory filenames: run00001.dat ---> "run%05d".dat
 PATTERN:=run%05d
-# set step of printing out coordinates in trajectory
-DT_NS=0.01 # time step of output for frame coordinates
+# set timestep used to record trajectory files
+DT_NS=0.01 # ns
 
 
 ## 3. specify fit parameters
 FIT_LIMIT_NS=1000 # ns
-# you may specify logariphmic resampling of the correlation function (LAG_INDEX="log") and set the corresponding number of points from 0 ns to FIT_LIMIT_NS (N_LAG_POINTS).
-# this step may prevent overfitting of the correlation function at large timescales
-# If  you want to fit without logariphmic spacing, you should delete the these parameters (LAG_SPACING and N_LAG_POINTS)
-LAG_SPACING="log" 
+# you may specify logarithmic resampling of the correlation function (LAG_INDEX="log") and set the number of points (N_LAG_POINTS) to cover the range from 0 ns to FIT_LIMIT_NS.
+# this choice may prevent overfitting of the correlation function at long timescales
+# if you want to fit the correlation function without the logarithmic resampling, you should delete these parameters (LAG_SPACING and N_LAG_POINTS)
+LAG_SPACING="log"
 N_LAG_POINTS=1000
 
 
-## 4. specify experimental parameters needed for calculation
+## 4. specify experimental parameters needed for calculations
 NMR_FREQ=850e6 # Hz
 TUMBLING_TIME=163.4 # ns
